89130000d089e085a49f3dcdad44a798666e6645
Eran Meir <eranmeir@gmail.com>
2015-09-17 05:22:14 +0300

    DOH
    remove debug prints

--- mlton.rb
+++ mlton.rb
@@ -34,9 +34,7 @@ class Mlton < Formula
       mv "man", "share"
 
       # Fix libgmp reference in mlton
-      puts "WD: " + Dir.pwd
       file_name = Dir.pwd + "/bin/mlton"
-      puts "file name: " + file_name
       text = File.read(file_name)
       new_contents = text.gsub(/lm \/opt\/local/, "lm /usr/local")
       File.open(file_name, "w") {|file| file.puts new_contents }
