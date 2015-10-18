87ce09e08199e3aa175fb461b41ea6f519ca976e
Eran Meir <eranmeir@gmail.com>
2015-10-19 00:36:02 +0300

    Updated to version 20151018
    Also improved formula conformance a bit

--- urweb.rb
+++ urweb.rb
@@ -1,31 +1,28 @@
 class Urweb < Formula
   desc "Ur/Web programming language"
   homepage "http://www.impredicative.com/ur/"
-  url "http://www.impredicative.com/ur/urweb-20150819.tgz"
-  version "20150819"
-  sha256 "d694b951b05549b6a1b9a2a3430e2660fb4694c324a9386610a17fa506a6ed3e"
+  url "http://www.impredicative.com/ur/urweb-20151018.tgz"
+  # version "20151018"
+  sha256 "c05f3aba7d617b1f7704c0810d51074bd44a889f5e59c8da7f82f650cb15e522"
 
-  # depends_on "cmake" => :build
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
   depends_on "openssl" => :build
-  depends_on 'mlton'
+  depends_on "mlton"
   depends_on :postgresql => :optional
   depends_on :mysql => :optional
 
-
   def install
-
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
                           "--disable-silent-rules",
                           "--prefix=#{prefix}"
 
-    system "make", "install" # if this fails, try separate make/make install steps
+    system "make", "install"
   end
 
   test do
-    system "false"
+    system "#{bin}/urweb"
   end
 end
