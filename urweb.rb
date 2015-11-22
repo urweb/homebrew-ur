098ac0439b44aaec511bf22c1d024645e8e66b10
Eran Meir <eranmeir@gmail.com>
2015-11-23 01:18:50 +0200

    Updated to version 20151122
    Also explicitly providing openssl headers path to ./configure

--- urweb.rb
+++ urweb.rb
@@ -1,9 +1,8 @@
 class Urweb < Formula
   desc "Ur/Web programming language"
   homepage "http://www.impredicative.com/ur/"
-  url "http://www.impredicative.com/ur/urweb-20151018.tgz"
-  # version "20151018"
-  sha256 "c05f3aba7d617b1f7704c0810d51074bd44a889f5e59c8da7f82f650cb15e522"
+  url "http://www.impredicative.com/ur/urweb-20151122.tgz"
+  sha256 "de4f3b26e5449f5f82cb9d02d19eb3f3e6187742ccc5d27aa1183c4ab6910f8f"
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
@@ -17,6 +16,7 @@ class Urweb < Formula
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
                           "--disable-silent-rules",
+                          "--with-openssl=/usr/local/opt/openssl",
                           "--prefix=#{prefix}"
 
     system "make", "install"
