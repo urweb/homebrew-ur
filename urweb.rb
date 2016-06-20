7be47a324d00e4f3c95819f6cb843f13673a111a
Eran Meir <eranmeir@gmail.com>
2016-06-20 04:19:59 +0300

    Adding gmp dependency + fixing brew audit errors

--- urweb.rb
+++ urweb.rb
@@ -4,22 +4,26 @@ class Urweb < Formula
   url "http://www.impredicative.com/ur/urweb-20160515.tgz"
   sha256 "58c5cc0e96f0c311de468b4475e4bf595e6de5c04095136eed4148545e70442f"
 
-
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
-  depends_on "openssl"
   depends_on "mlton" => :build
+  depends_on "openssl"
+  depends_on "gmp"
   depends_on :postgresql => :optional
   depends_on :mysql => :optional
 
   def install
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--with-openssl=/usr/local/opt/openssl",
-                          "--prefix=#{prefix}"
+    args = %W[
+      --disable-debug
+      --disable-dependency-tracking
+      --disable-silent-rules
+      --with-openssl=#{Formula["openssl"].opt_prefix}
+      --prefix=#{prefix}
+      SITELISP=$prefix/share/emacs/site-lisp/urweb
+    ]
 
+    system "./configure", *args
     system "make", "install"
   end
 
